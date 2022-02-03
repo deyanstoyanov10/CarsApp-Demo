b07ea9218369792a32beee474b9efe6bd29c1ea3
Deyan Stoyanov <didko985@gmail.com>
2022-02-04 00:13:35 +0200

    Add Username validation handler

---
+++ CarsApp/CarsApp.Handlers/RegistrationValidation/UsernameValidationHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace CarsApp.Handlers.RegistrationValidation
+{
+    using Common;
+    using Data.Models;
+
+    using Microsoft.AspNetCore.Identity;
+
+    using static CarsApp.Models.Authentication.AuthenticationRecords;
+
+    public class UsernameValidationHandler : Handler<RegisterUserInputModel, AppUser>
+    {
+        private const string USERNAME_ALREADY_EXIST_MESSAGE = "Username already exists.";
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public UsernameValidationHandler(UserManager<AppUser> userManager) => _userManager = userManager;
+
+        public override async Task<Result<AppUser>> Execute(RegisterUserInputModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.username);
+
+            return user is not null ? USERNAME_ALREADY_EXIST_MESSAGE : await base.Execute(model);
+        }
+    }
+}
